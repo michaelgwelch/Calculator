ccf30b31281f65d90ca0a73ac5c5e25a880cd1e6
Michael Welch <michaelgwelch@gmail.com>
2012-07-28 11:05:37 -0500

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Michael Welch on 7/28/12.
+//  Copyright (c) 2012 Michael Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Michael Welch on 7/28/12.
+//  Copyright (c) 2012 Michael Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
