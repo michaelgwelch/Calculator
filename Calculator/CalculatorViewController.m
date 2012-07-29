77f13a01bce70170991c26191328224ba3a8be31
Michael Welch <michaelgwelch@gmail.com>
2012-07-29 08:22:55 -0500

    Finish rough implementation of multiply

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -9,26 +9,90 @@
 #import "CalculatorViewController.h"
 
 @interface CalculatorViewController ()
+ 
+@property (nonatomic) BOOL userIsInMiddleOfEnteringNumber;
+@property (nonatomic, strong) NSMutableArray *stack;
 
 @end
 
 @implementation CalculatorViewController
 
-- (void)viewDidLoad
+@synthesize display = _display;
+@synthesize userIsInMiddleOfEnteringNumber = _userIsInMiddleOfEnteringNumber;
+@synthesize stack = _stack;
+
+
+- (void) setStack:(NSMutableArray *)stack
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _stack = stack;
 }
 
-- (void)viewDidUnload
+- (NSMutableArray *) stack
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    if (!_stack) [self setStack:[[NSMutableArray alloc] init]];
+    return _stack;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+- (void) push: (double) value
 {
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    [self.stack addObject:[NSNumber numberWithDouble:value]];
 }
+
+- (double) pop
+{
+    double value = [[self.stack lastObject] doubleValue];
+    [self.stack removeLastObject];
+    return value;
+}
+
+- (IBAction)digitPressed:(UIButton *)sender
+{
+    NSString *currentDisplay = self.display.text;
+    NSString *digit = sender.currentTitle;
+    
+    if (self.userIsInMiddleOfEnteringNumber)
+    {
+        currentDisplay = [currentDisplay stringByAppendingFormat:@"%@",digit];
+    }
+    else
+    {
+        currentDisplay = digit;
+        self.userIsInMiddleOfEnteringNumber = YES;
+    }
+    
+    self.display.text = currentDisplay;
+}
+
+- (IBAction)enterPressed
+{
+    self.userIsInMiddleOfEnteringNumber = NO;
+    double value = [self.display.text doubleValue];
+    [self push:value];
+}
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if (self.userIsInMiddleOfEnteringNumber) [self enterPressed];
+    
+    NSString *operation = sender.currentTitle;
+    double op1 = [self pop];
+    double op2 = [self pop];
+    double result;
+    
+    if ([operation isEqualToString:@"*"])
+    {
+        result = op1 * op2;
+    }
+    else if ([operation isEqualToString:@"/"])
+    {
+        
+    }
+    
+    [self push:result];
+    self.display.text = [[NSNumber numberWithDouble:result] stringValue];
+    
+}
+
+
 
 @end
