02263a93b3e858d24c59800eac4b4df76f23ac97
Michael Welch <michaelgwelch@gmail.com>
2012-08-26 10:57:00 -0500

    Add skeleton for Brain unit tests

---
+++ UnitTests/CalculatorBrainTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrainTests.h
+//  Calculator
+//
+//  Created by Michael Welch on 8/25/12.
+//  Copyright (c) 2012 Michael Welch. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorBrainTests : SenTestCase
+
+@end
---
+++ UnitTests/CalculatorBrainTests.m
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrainTests.m
+//  Calculator
+//
+//  Created by Michael Welch on 8/25/12.
+//  Copyright (c) 2012 Michael Welch. All rights reserved.
+//
+
+#import "CalculatorBrainTests.h"
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrainTests
+
+
+
+@end
